296ca1e9eb9d149b09c2f5be5cb5f073ec0b125d
sweetpeps <39718933+sweetpeps@users.noreply.github.com>
2021-01-16 08:27:05 +1100

    V1.1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <?php
+if (strpos($_SERVER["SERVER_NAME"], ".")) {
 list($subdomain,$host) = explode('.', $_SERVER["SERVER_NAME"]);
+
 $url = 'http://' . $subdomain . '.onion/';
 $request = $_SERVER['REQUEST_URI'];
 $ch = curl_init();
@@ -35,4 +37,9 @@ $headerr = curl_getinfo($ch);
 curl_close($ch);
 header('Content-Type: '.$headerr['content_type']);
 echo  $remoteSite;
+}
+else {
+  echo "Hello, if you want to use this site you must put the onion link before this sites link as a sub domain and remove the .onion\n\n";
+  echo "If you want to support me please send bitcoin to: 1EN41NQmVeGrwYoYYHtZ4yMTvCib6wG3SZ";
+}
 ?>
